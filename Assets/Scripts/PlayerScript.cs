0ccb479d4ab9ef52b9bc163c051cc62932569457
LD9-TAIKIOSHIDA\ossy3 <ar.tmu.ossy@gmail.com>
2017-04-27 15:34:03 +0900

    hatu

---
+++ Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerScript : MonoBehaviour {
+	
+	public static int score = 0;
+	public Text scoreText;
+
+	void Update () {
+
+		if (Input.GetMouseButtonDown (0)) {
+			GetComponent<Rigidbody> ().velocity = new Vector3 (0, 6, 0);
+			GetComponent<AudioSource> ().Play ();
+		}
+		scoreText.text = "Score : " + score.ToString ();
+	}
+
+	void OnTriggerExit(Collider other){
+		score++;
+	}
+
+	void OnCollisionEnter(Collision other){
+
+		SceneManager.LoadScene ("result");
+	}
+
+}
